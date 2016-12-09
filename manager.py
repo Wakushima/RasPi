05e77996f15fc2ccacebcde494f6c4a77196a54a
yh1126 <cake_sk_yh@icloud.com>
2016-12-09 12:12:10 +0900

    fixed bugs

--- manager.py
+++ manager.py
@@ -8,22 +8,30 @@ import RPi.GPIO as GPIO
 import os
 import datetime
 import event
-import motion
-import shoot
+import motion_sensor
+import camera
 import uploader
+import threading
+import sonic_sensor
+
+rlock = threading.RLock() # lock object
 
 class Manager:
 
   def __init__(self):
-     self.evt    = event.Event()
-     self.motion = motion.Motion() #actuator class
-     self.shoot  = shoot.Shoot() #sencer class
-     self.uploader = uploader.Uploader() #sencer class
-     self.motion.evt += self.shoot.execute
+     self.evt         = event.Event()
+     self.motion      = motion_sensor.MotionSensor() #actuator class
+     self.sonic       = sonic_sensor.SonicSensor()
+     self.camera       = camera.Camera() #sensor class
+     self.uploader    = uploader.Uploader() #process class
+     self.motion.evt += self.camera.execute
      self.motion.evt += self.uploader.execute
+     self.sonic.evt  += self.camera.execute
+     self.sonic.evt  += self.uploader.execute 
 
   def execute(self):
     self.motion.execute(self, None)
+    #self.sonic.measure(self, None)
 
 if __name__ == '__main__':
   man  = Manager()
--- uploader.py
+++ uploader.py
@@ -4,8 +4,11 @@
 # upload.py
 
 import pysftp
+import threading
 
-class Upload(object):
+up_lock = threading.RLock()
+
+class Uploader(object):
 
   def __init__(self):
     self.HOST = '172.21.42.152'
@@ -13,21 +16,24 @@ class Upload(object):
     self.USER = 'iwatalab'
     self.PRIVATE_KEY_FILE = '/home/pi/.ssh/id_rsa'
     self.uploadPath = '/home/iwatalab/upload_image'
+    global uploader_lock
 
   def execute(self, sender, earg):
-    print("upload_self", self)
-    print("upload_sender",sender)
-    print("upload_earg",earg)
-    self.todaydetail = earg
-    self.uploadPath = '/home/iwatalab/upload_image'
-    self.fPath = '/home/pi/iot_iwataken/motion_system/beagleboneblack/' + self.todaydetail + '.jpg'
-    sftp = pysftp.Connection(self.HOST, username=self.USER, port=self.PORT, private_key=self.PRIVATE_KEY_FILE)
-    sftp.listdir()
-    sftp.chdir(self.uploadPath)
-    sftp.getcwd()
-    sftp.put(self.fPath)
-    for item in sftp.execute('ls -l /home/iwatalab/upload_image'):
-      print item,     #結果を表示
-
-    sftp.close()
+#    try:
+    with up_lock:
+      #print("-- upload_Lock by " + threading.currentThread().getName() + " --")
+      self.uploadPath = '/home/iwatalab/upload_image'
+      self.fPath = '/home/pi/iot_iwataken/motion_system/beagleboneblack/' + earg + '.jpg' 
+      sftp = pysftp.Connection(self.HOST, username=self.USER, port=self.PORT, private_key=self.PRIVATE_KEY_FILE)
+      sftp.listdir()
+      sftp.chdir(self.uploadPath)
+      sftp.getcwd()
+      sftp.put(self.fPath)
+      for item in sftp.execute('ls -l /home/iwatalab/upload_image'):
+        print item,     #結果を表示
+  
+      sftp.close()
+#    print("----upload_UnLock---")
+#    except:
+#      print("cancel upload")
 
